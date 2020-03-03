513b28466aaa6c371292b17abd24b45eff564ab3
paul.montoya <paul.montoya@2bcore.com>
2020-03-03 09:11:50 -0600

    se modifica controller

--- src/main/java/demo/GreetingController.java
+++ src/main/java/demo/GreetingController.java
@@ -3,12 +3,11 @@ package demo;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api")
+//@RequestMapping("api")
 public class GreetingController {
 	
 	private static final String template = "Hola, %s!";
